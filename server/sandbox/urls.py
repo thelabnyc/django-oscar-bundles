97c7aa80a0f29a9de6ea0c934cc4d2cd2f9a7aab
Craig Weber <crgwbr@gmail.com>
2021-07-22 11:04:31 -0400

    Refs #17848 Fix deprecation warnings

--- server/sandbox/urls.py
+++ server/sandbox/urls.py
@@ -1,20 +1,23 @@
 from django.apps import apps
 from django.conf import settings
-from django.conf.urls import include, url, i18n
+from django.conf.urls import include, i18n
+from django.urls import re_path
 from django.contrib import admin
 from django.views.static import serve
 
 MEDIA_SETTINGS = {"document_root": settings.MEDIA_ROOT, "show_indexes": True}
 
 urlpatterns = [
-    url(r"^i18n/", include(i18n)),
-    url(r"^admin/", admin.site.urls),
-    url(r"^media/(?P<path>.*)$", serve, MEDIA_SETTINGS),
+    re_path(r"^i18n/", include(i18n)),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^media/(?P<path>.*)$", serve, MEDIA_SETTINGS),
     # Oscar Bundles API URLs
-    url(r"^api/", include(apps.get_app_config("oscarbundles_api").urls[0])),
-    url(r"^api/", include("oscarapi.urls")),
+    re_path(r"^api/", include(apps.get_app_config("oscarbundles_api").urls[0])),
+    re_path(r"^api/", include("oscarapi.urls")),
     # Oscar Bundles Dashboard URLs
-    url(r"^dashboard/", include(apps.get_app_config("oscarbundles_dashboard").urls[0])),
+    re_path(
+        r"^dashboard/", include(apps.get_app_config("oscarbundles_dashboard").urls[0])
+    ),
     # Include stock Oscar
-    url(r"^", include(apps.get_app_config("oscar").urls[0])),
+    re_path(r"^", include(apps.get_app_config("oscar").urls[0])),
 ]
